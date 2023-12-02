e0662f26a9a2e0ddb2ec922804d7b2f3cc370b8d
Matouš Kučera <mk@kcra.me>
2023-12-02 13:43:27 +0100

    refactor: make day 2 great again

--- src/main/kotlin/day2B.kt
+++ src/main/kotlin/day2B.kt
@@ -1,21 +1,19 @@
 fun main() = println(
     generateSequence(::readLine)
         .map { l ->
-            l.split(':')
-                .let { (_, v) ->
-                    v.split(';')
-                        .flatMap { d ->
-                            d.split(',').map { g ->
-                                g.filterNot(Char::isWhitespace)
-                                    .partition { !it.isDigit() }
-                                    .run { first to second.toInt() }
-                            }
-                        }
-                        .groupBy { it.first }
-                        .values
-                        .map { b -> b.maxOf { it.second } }
-                        .reduce(Int::times)
+            l.substringAfter(':')
+                .split(';')
+                .flatMap { d ->
+                    d.split(',').map { g ->
+                        g.filterNot(Char::isWhitespace)
+                            .partition { !it.isDigit() }
+                            .run { first to second.toInt() }
+                    }
                 }
+                .groupBy { it.first }
+                .values
+                .map { b -> b.maxOf { it.second } }
+                .reduce(Int::times)
         }
         .reduce(Int::plus)
 )
